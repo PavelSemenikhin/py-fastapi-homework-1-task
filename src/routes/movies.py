2fece12a33ea19b453eb38b5c7c7b499b87746bc
Pavlo Semenikhin <pashock1337@gmail.com>
2025-10-05 07:12:41 +0300

    solution py fastapi homework 1 task

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -2,9 +2,49 @@ from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database import get_db, MovieModel
-
+from src.database.session import get_db
+from src.database.models import MovieModel
+from src.schemas.movies import (
+    MovieDetailResponseSchema,
+    MovieListResponseSchema
+)
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+async def get_detail_movie(movie_id: int, db: AsyncSession = Depends(get_db)):
+    res = await db.execute(select(MovieModel).filter(MovieModel.id == movie_id))
+    db_movie = res.scalars().one_or_none()
+    if db_movie is None:
+        raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
+    return db_movie
+
+
+@router.get("/movies/", response_model=MovieListResponseSchema)
+async def get_list_movies(
+        page: int = Query(1, ge=1),
+        per_page: int = Query(10, ge=1, le=20),
+        db: AsyncSession = Depends(get_db)):
+    offset = (page - 1) * per_page
+    res = await db.execute(select(MovieModel).offset(offset).limit(per_page))
+    movies = res.scalars().all()
+
+    if not movies:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    count_result = await db.execute(select(MovieModel))
+    total_items = len(count_result.scalars().all())
+    total_pages = (total_items + per_page - 1) // per_page
+
+    base_url = "/theater/movies/"
+    prev_page = f"{base_url}?page={page - 1}&per_page={per_page}" if page > 1 else None
+    next_page = f"{base_url}?page={page + 1}&per_page={per_page}" if page < total_pages else None
+
+    return MovieListResponseSchema(
+        movies=movies,
+        prev_page=prev_page,
+        next_page=next_page,
+        total_pages=total_pages,
+        total_items=total_items
+    )
